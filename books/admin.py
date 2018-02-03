3e5c44cb74a0fd11448f68cd6f4c55b86b537b95
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-02-03 10:00:49 +0000

    Books funcional

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
+
+from django.contrib.auth.models import Permission
+admin.site.register(Permission)
+
+
 # Register your models here.
 from .models import Author, Book
 
 admin.site.register(Author)
 admin.site.register(Book)
+
+#Removes default delete_selected action
+admin.site.disable_action('delete_selected')
