cd89b04c08fcd6527c2f746f07f0fee30455382b
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-02-03 12:04:14 +0000

    Now detects if its authenticated

--- dj_books/dj_books/settings.py
+++ dj_books/dj_books/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+LOGIN_REDIRECT_URL = '/index/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
