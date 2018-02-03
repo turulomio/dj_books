64a53dfe1037f0989092b3c25da2a46a7abd05d3
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-02-03 08:05:28 +0000

    Added table editing with names in admin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Authors, Books
+from .models import Author, Book
 
-admin.site.register(Authors)
-admin.site.register(Books)
+admin.site.register(Author)
+admin.site.register(Book)
