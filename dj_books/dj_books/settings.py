b88cae6299f83d6069e2054c01a49399ba956d24
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-02-03 07:17:33 +0000

    Funcionando hello world

--- dj_books/dj_books/settings.py
+++ dj_books/dj_books/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dj_books.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'books',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
