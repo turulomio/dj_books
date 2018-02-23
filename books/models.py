39dbae5929c88d69e7fac0cf66a73faf77a9c1da
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-02-23 09:21:50 +0000

    Adding profile system

--- books/models.py
+++ books/models.py
@@ -7,7 +7,9 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 from  django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.db import models
+from django.dispatch import receiver
 
 class Profile(models.Model):    
     SPANISH= 'ES'
@@ -23,7 +25,18 @@ class Profile(models.Model):
         choices=LANGUAGES,
         default=ENGLISH,
     )
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
     
 class Author(models.Model):
     id_authors = models.AutoField(primary_key=True)
