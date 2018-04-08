1e2881d6d81c343ad7e0508fc8a690a7a174e26c
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-04-08 19:52:01 +0000

    Improving tables

--- books/tables.py
+++ books/tables.py
@@ -22,16 +22,16 @@ class TableEasyAuthors(TableEasyFromModel):
     def __init__(self,  queryset):
         TableEasyFromModel.__init__(self, Author,  queryset)
         self.setFields("id", ["name", "family_name", "birth", "death"])
-        self.setIBM("/books/author/new/", None, None)
+        self.setIBM("/books/author/new/", "/books/author/###/", "/books/author/###/delete/")
 
 class TableEasyBooks(TableEasyFromModel):
     def __init__(self,  queryset):
         TableEasyFromModel.__init__(self, Book,  queryset)
         self.setFields("id", ["title", "author", "year"])
-        self.setIBM("/books/book/new/", None, None)
+        self.setIBM("/books/book/new/", "/books/book/###/", "/books/book/###/delete/")
 
 class TableEasyValorations(TableEasyFromModel):
     def __init__(self,  queryset):
         TableEasyFromModel.__init__(self, Valoration,  queryset)
         self.setFields("id", ["book", "read_start", "read_end", "valoration"])
-        self.setIBM("/books/valoration/new/", None, None)
+        self.setIBM("/books/valoration/new/", "/books/valoration/###/", "/books/valoration/###/delete/")
