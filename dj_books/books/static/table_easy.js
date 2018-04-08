bdbf8c986ae7f3f6a59f12bd780b93960197e3c0
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-04-08 19:52:01 +0000

    Improving tables

--- dj_books/books/static/table_easy.js
+++ dj_books/books/static/table_easy.js
@@ -4,14 +4,16 @@ function myFunction() {
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+  for (i = 1; i < tr.length; i++) {
+    var s="";
+    td = tr[i].getElementsByTagName("td");
+    for (j=0;j<td.length;j++){
+        s=s+tr[i].getElementsByTagName("td")[j].innerHTML+" ";
+    }
+      if (s.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
       }
-    }       
   }
 }
--- dj_books/books/table_easy.py
+++ dj_books/books/table_easy.py
@@ -48,28 +48,34 @@ class TableEasyFromModel(TableEasy):
         TableEasy.__init__(self)
         self.model=model
         self.queryset=queryset
+        
+    def html_with_pk(self, html):
+        return html.replace("###",self.fields_pk)
+        
     
     def render(self):
         r='<div class="EasyTable">\n'
         ##Search box
         r=r+"""<button type="button" class="EasyTableButton" name="cmd_insert" onclick="window.location.href='{}';" >{}</button>\n""".format(self._html_insert, _("Insert"))
-        r=r+'<button type="button" class="EasyTableButton" name="cmd_update">{}</button>\n'.format(_("Update"))
-        r=r+'<button type="button" class="EasyTableButton" name="cmd_delete">{}</button>\n'.format(_("Delete"))
+        r=r+"""<button type="button" class="EasyTableButton" name="cmd_delete_selected" onclick="window.location.href='{}';" >{}</button>\n""".format(self._html_insert, _("Delete selected"))
         r=r+'    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">\n'
         r=r+'<table id="myTable">\n'
         r=r+'<tr class="header">\n'
         ##Header
-        r=r+'<td><input type="checkbox" id="all"/></td>\n'
+        r=r+'<th><input type="checkbox" id="all"/></th>\n'
         for field in self.fields:
             r=r+"<th>{}</th>\n".format(field.verbose_name)
+        r=r+'<th>{}</th>\n'.format(_("Actions"))
         r=r+"        </tr>\n"
         ##Data
-        r=r+'        <tr>\n'
         for o in self.queryset:
+            pk_id=getattr(o, self.fields_pk.name, None)
             r=r+"        <tr>\n"
-            r=r+'<td><input type="checkbox" id="{}"/></td>\n'.format(getattr(o, self.fields_pk.name, None)) 
+            r=r+'<td><input type="checkbox" id="{}"/></td>\n'.format(pk_id) 
             for field in self.fields:
                 r=r+"<td>{}</td>\n".format(getattr(o, field.name, None)) 
+            r=r+"""<td><button type="button" class="EasyTableButton" name="cmd_update"  onclick="window.location.href='{}';">{}</button>""".format(self._html_update.replace("###",str(pk_id)), _("Update"))
+            r=r+"""<button type="button" class="EasyTableButton" name="cmd_delete"  onclick="window.location.href='{}';">{}</button></td>\n""".format(self._html_delete.replace("###",str(pk_id)), _("Delete"))
             r=r+"        </tr>\n"
         r=r+"    </table>\n"
         r=r+"</div>\n"
