35ccaa31918a35be6e9cf41558561f4101edd187
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-06-04 07:02:07 +0000

    Added sitetree. Js original

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,22 @@
-from django.contrib import admin
-from dj_books.admin import mysite
+## Se mete en books  porque necesita los modelos
+
+
+from django.utils.translation import ugettext_lazy
+from books.models import Author, Book,  Profile
 from django.contrib.auth.models import Permission,  Group, User
-from .models import Author, Book,  Profile
 from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin# Need to import this since auth models get registered on import.
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+# Text to put at the end of each page's <title>.
+admin.site.site_title = ugettext_lazy('My personal Django')
+
+# Text to put in each page's <h1> (and above login form).
+admin.site.site_header = ugettext_lazy('My personal Django')
+
+# Text to put at the top of the admin index page.
+admin.site.index_title = ugettext_lazy('My personal Django administration')
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'family_name', 'birth', 'death')
@@ -19,7 +33,12 @@ class ProfileInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
+    
 
+class PermissionAdmin(admin.ModelAdmin):
+    model = Permission
+    fields = ['name']
+    
 class CustomUserAdmin(UserAdmin):
     inlines = (ProfileInline, )
 
@@ -28,14 +47,14 @@ class CustomUserAdmin(UserAdmin):
             return list()
         return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
-
-mysite.register(User, CustomUserAdmin)
-mysite.register(Group)
-mysite.register(Permission)
-mysite.register(Author, AuthorAdmin)
-mysite.register(Book, BookAdmin)
-mysite.logout_template='admin/login.html'
-mysite.site_url ="/home"
+admin.site.register(Permission, PermissionAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Group)
+    
+admin.site.logout_template='admin/login.html'
+admin.site.site_url ="/home"
 
 #Removes default delete_selected action
-mysite.disable_action('delete_selected')
+admin.site.disable_action('delete_selected')
