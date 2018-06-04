95ddf7e71bad6cb8f08770efd28c6279a1616aa8
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-06-04 20:48:30 +0000

    Removed djangotables y djangositetree

--- dj_books/books/tables.py
+++ dj_books/books/tables.py
@@ -1,23 +1,7 @@
-import django_tables2 as tables
 from django.utils.translation import gettext_lazy as _
 from .models import Author, Book, Valoration
 from books.table_easy import TableEasyFromModel
 
-class AuthorTable(tables.Table):
-    class Meta:
-        model = Author
-        template_name = 'django_tables2/table.html'
-        exclude = ("id",)
-        my_column = tables.TemplateColumn(verbose_name=_('My Column'), template_name="home.html")
-
-
-class BookTable(tables.Table):
-    class Meta:
-        model = Book
-        template_name = 'django_tables2/semantic.html'
-        exclude = ("id",)
-        
-        
 class TableEasyAuthors(TableEasyFromModel):
     def __init__(self,  queryset):
         TableEasyFromModel.__init__(self, Author,  queryset)
@@ -33,5 +17,5 @@ class TableEasyBooks(TableEasyFromModel):
 class TableEasyValorations(TableEasyFromModel):
     def __init__(self,  queryset):
         TableEasyFromModel.__init__(self, Valoration,  queryset)
-        self.setFields("id", ["book", "read_start", "read_end", "valoration"])
+        self.setFields("id", ["book", "read_start", "read_end", "valoration","user"])
         self.setIBM("/books/valoration/new/", "/books/valoration/###/", "/books/valoration/###/delete/")
