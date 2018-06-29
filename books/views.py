00542a6cd7bef6b4d5095067bf3ac62844e66468
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-06-29 15:23:53 +0000

    Code has been relocated

--- books/views.py
+++ books/views.py
@@ -1,2 +1,136 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required, permission_required
+from django.utils.translation import gettext_lazy as _
+from django.utils.decorators import method_decorator
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.shortcuts import render,  get_object_or_404
+from books.models import Author,  Book, Valoration
+from books.tables import TableEasyAuthors,  TableEasyValorations, TableEasyBooks
 
-# Create your views here.
+
+## @todo Add search to search field to repeat search
+## @todo Limit search minimum 3 and maximum 50
+## @todo Add a tab Widget, author, books, valorations with number in ttab
+def home(request):
+    search = request.GET.get('search')
+    if search!=None:
+        searchtitle=_("Looking for '{}' in Library database".format(search))
+        books=Book.objects.filter(Q(title__icontains=search) | Q(year__icontains=search))
+        authors=Author.objects.filter(Q(name__icontains=search) | Q(family_name__icontains=search))
+        if request.user.has_perm('books.search_valoration'):
+            valorations=Valoration.objects.filter(comment__icontains=search)
+    return render(request, 'home.html', locals())
+
+@login_required
+def database(request):
+    authors= Author.objects.order_by('name')
+    books=Book.objects.order_by('title')
+    valorations=Valoration.objects.order_by('read_end')
+    tableeasy_authors=TableEasyAuthors(authors)
+    tableeasy_books=TableEasyBooks(books)
+    tableeasy_valorations=TableEasyValorations(valorations)
+    return render(request, 'database.html', locals())
+
+@login_required
+def valoration(request):
+    valorations= Valoration.objects.order_by('read_start')
+    tableeasy_valorations=TableEasyValorations(valorations)
+    return render(request, 'valoration.html', locals())
+    
+
+def book_read(request, pk):
+    book=get_object_or_404(Book, pk=pk)
+    valorations=Valoration.objects.filter(book=book)
+    return render(request, 'books/book_read.html', locals())
+    
+def author_read(request, pk):
+    author=get_object_or_404(Author, pk=pk)
+    books=Book.objects.filter(author=author)
+    return render(request, 'books/author_read.html', locals())
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.add_author',raise_exception=True), name='dispatch')
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['name', 'family_name', 'birth', 'death']
+    template_name="books/author_edit.html"
+
+    def get_success_url(self):
+        return reverse_lazy('author-read',args=(self.object.id,))
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.change_author',raise_exception=True), name='dispatch')
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['name', 'family_name', 'birth', 'death']
+    template_name="books/author_edit.html"
+    success_url = reverse_lazy('home')
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.delete_author',raise_exception=True), name='dispatch')
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('home')
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.add_book',raise_exception=True), name='dispatch')
+class BookCreate(CreateView):
+    model = Book
+    fields = ['title', 'year', 'author']
+    template_name="books/book_edit.html"
+
+    def get_success_url(self):
+        return reverse_lazy('author-read',args=(self.object.author.id,))
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.change_book',raise_exception=True), name='dispatch')
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title', 'year', 'author']
+    template_name="books/book_edit.html"
+    success_url = reverse_lazy('home')
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('books.delete_book',raise_exception=True), name='dispatch')
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('home')
+
+
+
+
+## @todo user must not be asked
+class ValorationCreate(CreateView):
+    model = Valoration
+    fields = ['book', 'user', 'comment','valoration','read_start','read_end']
+    template_name="books/valoration_edit.html"
+    success_url = reverse_lazy('valoration-list')
+
+    def get_form(self, form_class=None): 
+        if form_class is None: 
+            form_class = self.get_form_class()
+        form = super(ValorationCreate, self).get_form(form_class)
+        form.fields['read_start'].widget.attrs['class'] ='datepicker'
+        form.fields['read_end'].widget.attrs['class'] ='datepicker'
+        return form
+
+class ValorationUpdate(UpdateView):
+    model = Valoration
+    fields = ['book', 'user', 'comment','valoration','read_start','read_end']
+    template_name="books/valoration_edit.html"
+    success_url = reverse_lazy('valoration-list')
+
+    def get_form(self, form_class=None): 
+        if form_class is None: 
+            form_class = self.get_form_class()
+        form = super(ValorationUpdate, self).get_form(form_class)
+        form.fields['read_start'].widget.attrs['class'] ='datepicker'
+        form.fields['read_end'].widget.attrs['class'] ='datepicker'
+        return form
+
+class ValorationDelete(DeleteView):
+    model = Valoration
+    success_url = reverse_lazy('valoration-list')
+
--- dj_books/urls.py
+++ dj_books/urls.py
@@ -19,63 +19,39 @@ from django.contrib import admin
 from django.urls import reverse_lazy
 
 ## @todo Change views import to a generic way
-from dj_books.views import  (
-    activate, 
-    account_activation_sent, 
-    author_read, 
-    book_read, 
-    home, 
-    database,  
-    signup, 
-    profile_edit,  
-    AuthorCreate,  
-    AuthorDelete,  
-    AuthorUpdate, 
-    BookCreate, 
-    BookUpdate, 
-    BookDelete, 
-    ValorationCreate, 
-    ValorationUpdate, 
-    ValorationDelete, 
-    valoration,
-    )
-
-## Return a reg expression appending WWWSUBDIR, from absoluth
-def ab(reg):
-    return reg
-    #return  r"{}{}{}".format(reg[:1], settings.WEBSUBDIR , reg[1:])
+from . import views as dj_books_views
+from books import views as books_view
 
 urlpatterns = [
-    url(r'^signup/$', signup, name='signup'),
+    url(r'^signup/$', dj_books_views.signup, name='signup'),
     
-    url(r'^account_activation_sent/$', account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',  activate, name='activate'),
+    url(r'^account_activation_sent/$', dj_books_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',  dj_books_views.activate, name='activate'),
     
-    url(ab(r'^admin/'), admin.site.urls,  name="admin-site"),
-    url(ab(r'^accounts/login/$'), auth_views.login, {'template_name': 'admin/login.html'}, name="login"), 
-    url(ab(r'^logout/$'), auth_views.logout, {'next_page': reverse_lazy('home')}, name="logout"), 
-    url(r'^$', home, name='home'),
-    url(ab(r'^database/$'), database, name='database'), 
-    url(ab(r'^profile/$'), profile_edit, name="profile"), 
+    url(r'^admin/', admin.site.urls,  name="admin-site"),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'admin/login.html'}, name="login"), 
+    url(r'^logout/$', auth_views.logout, {'next_page': reverse_lazy('home')}, name="logout"), 
+    url(r'^$', books_view.home, name='home'),
+    url(r'^database/$', books_view.database, name='database'), 
+    url(r'^profile/$', dj_books_views.profile_edit, name="profile"), 
 
-    url(ab(r'^books/author/create/$'), AuthorCreate.as_view(), name='author-add'),
-    url(r'^books/author/(?P<pk>\d+)/$', author_read, name='author-read'), 
-    url(ab(r'^books/author/(?P<pk>\d+)/update/$'), AuthorUpdate.as_view(), name='author-edit'),
-    url(ab(r'^books/author/(?P<pk>\d+)/delete/$'), AuthorDelete.as_view(), name='author-delete'),
+    url(r'^books/author/create/$', books_view.AuthorCreate.as_view(), name='author-add'),
+    url(r'^books/author/(?P<pk>\d+)/$', books_view.author_read, name='author-read'), 
+    url(r'^books/author/(?P<pk>\d+)/update/$', books_view.AuthorUpdate.as_view(), name='author-edit'),
+    url(r'^books/author/(?P<pk>\d+)/delete/$', books_view.AuthorDelete.as_view(), name='author-delete'),
 
-    url(ab(r'^books/book/create/$'), BookCreate.as_view(), name='book-add'),
-    url(ab(r'^books/book/(?P<pk>\d+)/$'), book_read, name='book-read'),
-    url(ab(r'^books/book/(?P<pk>\d+)/update/$'), BookUpdate.as_view(), name='book-edit'),
-    url(ab(r'^books/book/(?P<pk>\d+)/delete/$'), BookDelete.as_view(), name='book-delete'),
+    url(r'^books/book/create/$', books_view.BookCreate.as_view(), name='book-add'),
+    url(r'^books/book/(?P<pk>\d+)/$', books_view.book_read, name='book-read'),
+    url(r'^books/book/(?P<pk>\d+)/update/$', books_view.BookUpdate.as_view(), name='book-edit'),
+    url(r'^books/book/(?P<pk>\d+)/delete/$', books_view.BookDelete.as_view(), name='book-delete'),
 
-    url(ab(r'^books/valoration/list/'), valoration, name='valoration-list'),
-    url(ab(r'^books/valoration/create/$'), ValorationCreate.as_view(), name='valoration-add'),
-    url(ab(r'^books/valoration/(?P<pk>\d+)/update/$'), ValorationUpdate.as_view(), name='valoration-edit'),
-    url(ab(r'^books/valoration/(?P<pk>\d+)/delete/$'), ValorationDelete.as_view(), name='valoration-delete'),
+    url(r'^books/valoration/list/', books_view.valoration, name='valoration-list'),
+    url(r'^books/valoration/create/$', books_view.ValorationCreate.as_view(), name='valoration-add'),
+    url(r'^books/valoration/(?P<pk>\d+)/update/$', books_view.ValorationUpdate.as_view(), name='valoration-edit'),
+    url(r'^books/valoration/(?P<pk>\d+)/delete/$', books_view.ValorationDelete.as_view(), name='valoration-delete'),
 
-
-    url(r'^books/querys/last/', valoration, name='query-books-last'),
-    url(r'^books/querys/valued/', valoration, name='query-books-valued'),
+    url(r'^books/querys/last/', books_view.valoration, name='query-books-last'),
+    url(r'^books/querys/valued/', books_view.valoration, name='query-books-valued'),
 
 ]
 
