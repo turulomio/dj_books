7ba11c7a43d795cc1c99cbce407811848a0ffd66
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-06-29 10:29:16 +0000

    Mejorada traducción. Less field in signup

--- dj_books/forms.py
+++ dj_books/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from books.models import Profile
+from django.utils.translation import gettext_lazy as _
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -20,10 +21,8 @@ class ProfileForm(ModelForm):
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text=_('Required. Inform a valid email address.'))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2', )
