c5e46094e7f86f0ce584edb7e7cfb13fe5f5467f
turulomio <turulomio@7fa43128-5678-4d1b-8f21-0204ea82b914>
2018-06-29 15:23:53 +0000

    Code has been relocated

--- dj_books/dj_books/admin.py
+++ dj_books/dj_books/admin.py
@@ -1,1 +1,53 @@
+## @package admin
+## @brief Defines everything for Django Admin Site
 
+## Se mete en books  porque necesita los modelos
+
+
+from django.utils.translation import ugettext_lazy
+from books.models import  Profile
+from django.contrib.auth.models import Permission,  Group, User
+from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse_lazy
+from django.contrib import admin# Need to import this since auth models get registered on import.
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+## Text to put at the end of each page's <title>.
+admin.site.site_title = ugettext_lazy('My personal Django')
+
+## Text to put in each page's <h1> (and above login form).
+admin.site.site_header = ugettext_lazy('My personal Django')
+
+## Text to put at the top of the admin index page.
+admin.site.index_title = ugettext_lazy('My personal Django administration')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+    
+
+class PermissionAdmin(admin.ModelAdmin):
+    model = Permission
+    list_display = ['name','content_type','codename']
+    search_fields = ['name', 'content_type','codename']
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+admin.site.register(Permission, PermissionAdmin)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Group)
+    
+admin.site.site_url = reverse_lazy('home') 
+admin.site.logout_template='admin/login.html'
+
+
+#Removes default delete_selected action
+admin.site.disable_action('delete_selected')
--- dj_books/dj_books/views.py
+++ dj_books/dj_books/views.py
@@ -1,14 +1,11 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
-from django.utils.decorators import method_decorator
 from django.db import transaction
-from django.db.models import Q
 from django.urls import reverse_lazy
 from django.contrib.auth import update_session_auth_hash,  login
 from django.contrib.auth.forms import PasswordChangeForm
-from django.shortcuts import render,  get_object_or_404,  redirect
+from django.shortcuts import render,  redirect
 from django.contrib.sites.shortcuts import get_current_site
 
 from django.utils.encoding import force_bytes, force_text
@@ -18,39 +15,7 @@ from django.contrib.auth.models import User
 from .tokens import account_activation_token
 from django.contrib.auth.models import Group
 
-from books.models import Author,  Book, Valoration
-from .forms import UserForm, ProfileForm,  SignUpForm
-from books.tables import TableEasyAuthors,  TableEasyValorations, TableEasyBooks
-
-## @todo Add search to search field to repeat search
-## @todo Limit search minimum 3 and maximum 50
-## @todo Add a tab Widget, author, books, valorations with number in ttab
-def home(request):
-    search = request.GET.get('search')
-    if search!=None:
-        searchtitle=_("Looking for '{}' in Library database".format(search))
-        books=Book.objects.filter(Q(title__icontains=search) | Q(year__icontains=search))
-        authors=Author.objects.filter(Q(name__icontains=search) | Q(family_name__icontains=search))
-        if request.user.has_perm('books.search_valoration'):
-            valorations=Valoration.objects.filter(comment__icontains=search)
-    return render(request, 'home.html', locals())
-
-@login_required
-def database(request):
-    authors= Author.objects.order_by('name')
-    books=Book.objects.order_by('title')
-    valorations=Valoration.objects.order_by('read_end')
-    tableeasy_authors=TableEasyAuthors(authors)
-    tableeasy_books=TableEasyBooks(books)
-    tableeasy_valorations=TableEasyValorations(valorations)
-    return render(request, 'database.html', locals())
-
-@login_required
-def valoration(request):
-    valorations= Valoration.objects.order_by('read_start')
-    tableeasy_valorations=TableEasyValorations(valorations)
-    return render(request, 'valoration.html', locals())
-    
+from .forms import UserForm, ProfileForm,  SignUpForm    
 
 ## View to register a new user
 def signup(request):
@@ -100,17 +65,6 @@ def activate(request, uidb64, token):
     else:
         return render(request, 'account_activation_invalid.html')
 
-def book_read(request, pk):
-    book=get_object_or_404(Book, pk=pk)
-    valorations=Valoration.objects.filter(book=book)
-    return render(request, 'books/book_read.html', locals())
-    
-def author_read(request, pk):
-    author=get_object_or_404(Author, pk=pk)
-    books=Book.objects.filter(author=author)
-    return render(request, 'books/author_read.html', locals())
-
-
 @login_required
 @transaction.atomic
 def profile_edit(request):
@@ -141,90 +95,3 @@ def profile_edit(request):
         change_password_form = PasswordChangeForm(request.user)
     change_password_form.fields['old_password'].widget.attrs.pop("autofocus", None)
     return render(request, 'profile.html', locals())
-
-## @todo After inserting returns to home. It's ugly
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.add_author',raise_exception=True), name='dispatch')
-class AuthorCreate(CreateView):
-    model = Author
-    fields = ['name', 'family_name', 'birth', 'death']
-    template_name="books/author_edit.html"
-
-    def get_success_url(self):
-        return reverse_lazy('author-read',args=(self.object.id,))
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.change_author',raise_exception=True), name='dispatch')
-class AuthorUpdate(UpdateView):
-    model = Author
-    fields = ['name', 'family_name', 'birth', 'death']
-    template_name="books/author_edit.html"
-    success_url = reverse_lazy('home')
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.delete_author',raise_exception=True), name='dispatch')
-class AuthorDelete(DeleteView):
-    model = Author
-    success_url = reverse_lazy('home')
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.add_book',raise_exception=True), name='dispatch')
-class BookCreate(CreateView):
-    model = Book
-    fields = ['title', 'year', 'author']
-    template_name="books/book_edit.html"
-
-    def get_success_url(self):
-        return reverse_lazy('author-read',args=(self.object.author.id,))
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.change_book',raise_exception=True), name='dispatch')
-class BookUpdate(UpdateView):
-    model = Book
-    fields = ['title', 'year', 'author']
-    template_name="books/book_edit.html"
-    success_url = reverse_lazy('home')
-
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('books.delete_book',raise_exception=True), name='dispatch')
-class BookDelete(DeleteView):
-    model = Book
-    success_url = reverse_lazy('home')
-
-
-
-
-## @todo user must not be asked
-class ValorationCreate(CreateView):
-    model = Valoration
-    fields = ['book', 'user', 'comment','valoration','read_start','read_end']
-    template_name="books/valoration_edit.html"
-    success_url = reverse_lazy('valoration-list')
-
-    def get_form(self, form_class=None): 
-        if form_class is None: 
-            form_class = self.get_form_class()
-        form = super(ValorationCreate, self).get_form(form_class)
-        form.fields['read_start'].widget.attrs['class'] ='datepicker'
-        form.fields['read_end'].widget.attrs['class'] ='datepicker'
-        return form
-
-class ValorationUpdate(UpdateView):
-    model = Valoration
-    fields = ['book', 'user', 'comment','valoration','read_start','read_end']
-    template_name="books/valoration_edit.html"
-    success_url = reverse_lazy('valoration-list')
-
-    def get_form(self, form_class=None): 
-        if form_class is None: 
-            form_class = self.get_form_class()
-        form = super(ValorationUpdate, self).get_form(form_class)
-        form.fields['read_start'].widget.attrs['class'] ='datepicker'
-        form.fields['read_end'].widget.attrs['class'] ='datepicker'
-        return form
-
-class ValorationDelete(DeleteView):
-    model = Valoration
-    success_url = reverse_lazy('valoration-list')
