97aadc747a602ddae981f5aac9160efd7bdb2876
turulomio <turulomio@yahoo.es>
2020-08-10 23:23:46 +0200

    Fixed valoration urls

--- books/tables.py
+++ books/tables.py
@@ -29,9 +29,9 @@ class TableEasyValorations(TableEasyFromModel):
         TableEasyFromModel.__init__(self, "TableEasyValorations", Valoration,  queryset, request)
         self.setFields("id", ["book.title", "read_start", "read_end", "valoration","user.email", "comment"])
         self.setCRUDE(  "/books/valoration/create/", ['books.add_valoration'], 
-                                    "/books/valoration/###/", ['books.search_valoration'], 
-                                    "/books/valoration/###/update/", ['books.change_valoration'],  
-                                    "/books/valoration/###/delete/", ['books.delete_valoration'], 
+                                    "/books/valoration/read/###/", ['books.search_valoration'], 
+                                    "/books/valoration/update/###/", ['books.change_valoration'],  
+                                    "/books/valoration/delete/###/", ['books.delete_valoration'], 
                                     "", ['books.export_valoration'] )
         self.headers[0]=_("Book title")
         self.headers[4]=_("User mail")
