dea587569d5703b94d130b18b844d694bb982976
turulomio <turulomio@yahoo.es>
2020-08-14 07:56:37 +0200

    Added doxygen and procedure commands

--- books/management/commands/doxygen.py
+++ books/management/commands/doxygen.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from os import system, chdir
 from books.__init__ import __version__
@@ -6,10 +7,20 @@ class Command(BaseCommand):
     help = 'Create doxygen documentation'
 
     def handle(self, *args, **options):
+    
+    
         system("""sed -i -e "41d" doc/Doxyfile""")#Delete line 41
         system("""sed -i -e "41iPROJECT_NUMBER         = {}" doc/Doxyfile""".format(__version__))#Insert line 41
         chdir("doc")
         system("doxygen Doxyfile")
-        system("rsync -avzP -e 'ssh -l turulomio' html/ frs.sourceforge.net:/home/users/t/tu/turulomio/userweb/htdocs/doxygen/dj_books/ --delete-after")
+        db=settings.DATABASES['default']
+
+        chdir("html")
+        system("/usr/bin/postgresql_autodoc -d {} -h {} -u {} -p {} --password={} -t html".format(db['NAME'], db['HOST'], db['USER'], db['PORT'], db['PASSWORD']))
+        system("/usr/bin/postgresql_autodoc -d {} -h {} -u {} -p {} --password={} -t dot_shortfk".format(db['NAME'], db['HOST'], db['USER'], db['PORT'], db['PASSWORD']))
+        system("dot -Tpng {0}.dot_shortfk -o {0}_er.png".format(db['NAME']))
         chdir("..")
+
+
+        system("rsync -avzP -e 'ssh -l turulomio' html/ frs.sourceforge.net:/home/users/t/tu/turulomio/userweb/htdocs/doxygen/dj_books/ --delete-after")
 
